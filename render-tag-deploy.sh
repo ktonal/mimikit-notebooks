721a41abcee24a3dd05f794dc32f47de9172be87
Antoine Daurat <antoinedaurat@gmail.com>
2021-06-11 22:56:02 +0200

    clean up

---
+++ render-tag-deploy.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+pip install nbformat mimikit
+
+git checkout test
+
+python make_notebooks.py
+
+# grab mmk version
+export TAG="v$(pip list | grep mimikit |  awk '{print $2}')"
+
+git commit -am "$(TAG)"
+
+git tag -a $TAG -m "$(TAG)"
+
+#git push
+
