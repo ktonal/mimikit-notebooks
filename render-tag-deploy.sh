0f1b0d21498b08489cae8e188527971c6dcd5b3f
Antoine Daurat <antoinedaurat@gmail.com>
2021-06-11 23:01:13 +0200

    v0.2.0

--- render-tag-deploy.sh
+++ render-tag-deploy.sh
@@ -9,9 +9,9 @@ python make_notebooks.py
 # grab mmk version
 export TAG="v$(pip list | grep mimikit |  awk '{print $2}')"
 
-git commit -am "$(TAG)"
+git commit -am $TAG
 
-git tag -a $TAG -m "$(TAG)"
+git tag -a $TAG -m $TAG
 
 #git push
 
